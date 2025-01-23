5e821ad17263d05106395aa9df2a994fdc37b85c
Oliver Sieyes <oliversieyes@Olivers-MacBook-Air.local>
2025-01-23 16:45:45 +0100

    Initial commit

---
+++ HOMEWORK.py
@@ -0,0 +1,7 @@
+print("Im going to tell you a little bit about myself through the use of python code")
+print("i am", 2024-2004, "years old")
+print("Im from Denmark but i live in Switzerland with my family")
+hobbies = ["Skiing","Running", "Listening to music"]
+print("I have 3 main hobbies:")
+print(hobbies)
+print("I hope that this is what you meant in regards to a few lines of code :D")
